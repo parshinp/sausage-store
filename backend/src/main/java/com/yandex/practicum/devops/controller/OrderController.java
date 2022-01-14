6f556f11d0567fcd1d7d50c4d46b42a562426614
parshin <parshin@srwd.ru>
2022-01-14 22:36:20 +0000

    Update OrderController.java

--- backend/src/main/java/com/yandex/practicum/devops/controller/OrderController.java
+++ backend/src/main/java/com/yandex/practicum/devops/controller/OrderController.java
@@ -80,7 +80,7 @@ public class OrderController {
           .collect(Collectors.toList());
 
         if (!CollectionUtils.isEmpty(list)) {
-            new ResourceNotFoundException("Product not found");
+            throw new ResourceNotFoundException("Product not found");
         }
     }
 
