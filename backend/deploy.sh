08ba5d2be3e43487c1b1934105e4d387e825bd43
parshin <parshin@srwd.ru>
2022-02-13 20:20:28 +0000

    Update backend/sausage-store-backend.service, backend/deploy.sh, backend/.gitlab-ci.yml, .gitlab-ci.yml files

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,12 +3,15 @@
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
-sudo rm -f /home/${DEV_USER}/sausage-store.jar||true
-#Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-parshin_pavel-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 
-sudo cp ./sausage-store.jar /home/${DEV_USER}/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
+#sudo rm -f /home/${DEV_USER}/sausage-store.jar||true
+#Переносим артефакт в нужную папку
+#curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-parshin_pavel-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+
+#sudo cp ./sausage-store.jar /home/${DEV_USER}/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
+
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
+
 #Перезапускаем сервис сосисочной
 sudo systemctl restart sausage-store-backend.service 
