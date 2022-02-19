d2265896276b306e824c6e287d329de2c0f46a87
Pavel Parshin <parshin@srwd.ru>
2022-02-19 21:43:49 +0000

    terraform files were added

---
+++ terraform/provider.tf
@@ -0,0 +1,8 @@
+#provider file
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = var.yc_zone
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+# variables file
+variable "yc_token" {
+  description = "yandex cloud token"
+  type        = string
+  default     = "AQAEA7qh_PlhAATuwfgnK6_ghEqkj0q-hWVB9fM"
+}
+
+variable "yc_cloud_id" {
+  description = "yandex cloud id"
+  type        = string
+  default     = "b1g3jddf4nv5e9okle7p"
+}
+
+variable "yc_folder_id" {
+  description = "folder id"
+  type        = string
+  default     = "b1gr4fhu1lstvqs3ad4l"
+}
+
+variable "yc_zone" {
+  description = "yandex cloud zone"
+  type        = string
+  default     = "ru-central1-a"
+}
+
+variable "yc_res_name" {
+  description = "yandex cloud resource name"
+  type        = string
+  default     = "chapter5-lesson2-pavel-parshin"
+}
+
