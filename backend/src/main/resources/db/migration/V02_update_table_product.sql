9a5764c55b9902a48173d0f033097c9aec7a4028
parshin <parshin@srwd.ru>
2022-03-20 13:12:48 +0000

    Update backend/src/test/resources/application.properties, backend/src/main/resources/application.properties, backend/src/main/resources/db/migration/V02_update_table_product.sql, backend/src/main/resources/db/migration/R_grant_access.sql files

---
+++ backend/src/main/resources/db/migration/V02_update_table_product.sql
@@ -0,0 +1,2 @@
+ALTER TABLE product ALTER COLUMN name TYPE varchar(20);
+ALTER TABLE product ALTER COLUMN product_url TYPE varchar(100);
--- backend/src/main/resources/db/migration/V03_update_table_product.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE product ALTER COLUMN 'name' TYPE varchar(275);
-
-ALTER TABLE product ALTER COLUMN 'product_url' TYPE varchar(355);
