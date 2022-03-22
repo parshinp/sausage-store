ac45d0b82d368ffce974f04179d91dd1f7557f9f
Big Lebowski <noreply@biglebows.ki>
2022-03-22 21:05:13 +0000

    Added MongoDB integration

--- backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
+++ backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SausageApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/com/yandex/practicum/devops/task/ReportSaverTask.java
@@ -0,0 +1,37 @@
+package com.yandex.practicum.devops.task;
+
+import com.yandex.practicum.devops.model.Report;
+import com.yandex.practicum.devops.repository.ReportRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableScheduling
+public class ReportSaverTask {
+    Logger log = LoggerFactory.getLogger(ReportSaverTask.class);
+
+    public final ReportRepository reportRepository;
+
+    public final RestTemplate restTemplate = new RestTemplateBuilder().build();
+
+    public ReportSaverTask(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
+
+    @Scheduled(fixedDelay = 60000)
+    public void saveReport() {
+        log.info("Saving report");
+        Report report =
+                restTemplate.getForObject("https://d5dg7f2abrq3u84p3vpr.apigw.yandexcloud.net/report", Report.class);
+        if (report != null) {
+            reportRepository.save(report);
+        } else {
+            log.error("Report is empty");
+        }
+    }
+}
