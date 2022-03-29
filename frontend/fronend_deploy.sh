bd4bc49bf3a32c1ed10f4b842af2e32b6262e9f5
Pavel Parshin <parshin@srwd.ru>
2022-03-30 01:54:04 +0300

    docker-in-docker

---
+++ frontend/fronend_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set +e
+
+sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+sudo docker network create -d bridge sausage_network || true
+sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-frontend:latest
+sudo docker stop frontend || true
+sudo docker rm frontend || true
+set -e
+sudo docker run -d --name frontend \
+    --network=sausage_network \
+    --restart always \
+    --pull always \
+    gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-frontend:latest
