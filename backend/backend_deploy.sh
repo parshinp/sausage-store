4205e75148106de11ed6e8d901bd04cf098d0f83
Pavel Parshin <parshin@srwd.ru>
2022-03-31 03:37:04 +0300

    ci

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -4,8 +4,9 @@ cat > .env <<EOF
 SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
-SPRING_DATA_MONGODB_URI=mongodb://parshin:Testusr1234@rc1b-nczpq63snuc5a3z2.mdb.yandexcloud.net:27018/parshin?tls=true
+SPRING_DATA_MONGODB_URI=${MONGO_DATA}
 EOF
+pwd
 sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 sudo docker network create -d bridge sausage_network || true
 sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend:latest
