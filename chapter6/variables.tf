afe20a77fd970bf2dd19d1e071547106f4adde13
Pavel Parshin <parshin@srwd.ru>
2022-04-05 01:49:03 +0300

    s3 tfstate

--- chapter6/variables.tf
+++ chapter6/variables.tf
@@ -17,21 +17,8 @@ variable "yandex_cloud_students_2nd_stream" {
   default     = "b1gr4fhu1lstvqs3ad4l"
 }
 
-variable "ubuntu_20_04_lts_vgpu_v20211027" {
-  description = "Ubuntu 20.04 LTS VGPU v20211027"
-  type        = string
-  default     = "fd80qm01ah03dkqb14lc"
-}
-
 variable "zone_ru_central1_a" {
   description = "yandex cloud zone"
   type        = string
   default     = "ru-central1-a"
 }
-
-variable "yc_auto_created_default_subnet_id" {
-  description = "Auto-created def subnet 10.128.0.0/24 for zone ru-central1-a"
-  type        = string
-  default     = "e9bmsiue0hk5kl2dou85"
-}
-
