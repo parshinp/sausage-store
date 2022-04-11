942312f594893344b648bbee0fda30f9025d4f25
Pavel Parshin <parshin@srwd.ru>
2022-04-11 23:42:22 +0300

    deploy vault sh

--- vault/deploy_vault.sh
+++ vault/deploy_vault.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 set +e
 
-SPRING_VAULT_HOST=${SPRING_VAULT_HOST}
-SPRING_VAULT_TOKEN=${SPRING_VAULT_TOKEN}
 
-cat <<EOF | docker exec -i vault ash
-  sleep 10;
-  vault login ${SPRING_VAULT_TOKEN}
+sudo docker stop vault && sudo docker rm vault
+sudo docker run -d --cap-add=IPC_LOCK --name vault -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -e 'VAULT_SERVER=http://127.0.0.1:8200' -e 'VAULT_ADDR=http://127.0.0.1:8200' vault
 
-  vault secrets enable -path=secret kv
+cat <<EOF | sudo docker exec -i vault ash
+sleep 10;
+vault login myroot
 
-  vault kv put secret/sausage-store spring.datasource.username=sa \
-  spring.datasource.password=password \
-  spring.data.mongodb.uri=mongodb://parshin:Testusr1234@rc1b-nczpq63snuc5a3z2.mdb.yandexcloud.net:27018/parshin?tls=true \
-  spring.datasource.url=jdbc:postgresql://c-c9qslvj961omn1nevnjl.rw.mdb.yandexcloud.net:6432/testdb
+vault secrets enable -path=secret kv
+
+vault kv put secret/sausage-store spring.datasource.username=sa \
+spring.datasource.password=password \
+spring.data.mongodb.uri=mongodb://parshin:Testusr1234@rc1b-nczpq63snuc5a3z2.mdb.yandexcloud.net:27018/parshin?tls=true \
+spring.datasource.url=jdbc:postgresql://c-c9qslvj961omn1nevnjl.rw.mdb.yandexcloud.net:6432/testdb
 EOF
