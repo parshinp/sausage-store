f392c1999ceb951ebc1972314db93d08774bbb92
Pavel Parshin <parshin@srwd.ru>
2022-04-12 01:05:44 +0300

    whoohooo compose

--- backend-report/back_rep_compose_deploy.sh
+++ backend-report/back_rep_compose_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +e
-touch .env.back
-cat > .env.back <<EOF
+touch .env.report
+cat > .env.report <<EOF
 SPRING_VAULT_HOST=${SPRING_VAULT_HOST}
 SPRING_VAULT_TOKEN=${SPRING_VAULT_TOKEN}
 EOF
@@ -10,4 +10,4 @@ sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 #sudo docker-compose stop backend2 || true
 #sudo docker-compose rm backend2 || true
 set -e
-sudo docker-compose --env-file .env.back up -d --no-deps --build backend
+sudo docker-compose --env-file .env.report up -d --no-deps --build backend-report
--- front_compose_deploy.sh
+++ front_compose_deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set +e
-touch .env.back
-cat > .env.back <<EOF
-SPRING_VAULT_HOST=${SPRING_VAULT_HOST}
-SPRING_VAULT_TOKEN=${SPRING_VAULT_TOKEN}
-EOF
 sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 #sudo docker-compose build || true
 #sudo docker-compose stop backend2 || true
 #sudo docker-compose rm backend2 || true
 set -e
-sudo docker-compose --env-file .env.back up -d --no-deps --build backend
+sudo docker-compose up -d --no-deps --build frontend
