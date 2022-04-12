b3b14e0a29a54ac7358d24181b11b5c308954767
Pavel Parshin <parshin@srwd.ru>
2022-04-12 03:36:52 +0300

    compose

--- compose_front_deploy.sh
+++ compose_front_deploy.sh
@@ -6,8 +6,5 @@ cat > .env.front <<EOF
 VERSION=${VERSION}
 EOF
 sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-#sudo docker-compose build || true
-#sudo docker-compose stop backend2 || true
-#sudo docker-compose rm backend2 || true
 set -e
-sudo docker-compose --env-file .env.front up -d --no-deps --build frontend
+sudo docker-compose --env-file .env.front up -d --no-deps
--- compose_rep_deploy.sh
+++ compose_rep_deploy.sh
@@ -8,8 +8,5 @@ SPRING_VAULT_TOKEN=${SPRING_VAULT_TOKEN}
 VERSION=${VERSION}
 EOF
 sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-#sudo docker-compose build || true
-#sudo docker-compose stop backend2 || true
-#sudo docker-compose rm backend2 || true
 set -e
-sudo docker-compose --env-file .env.report up -d --no-deps --build backend-report
+sudo docker-compose --env-file .env.report up -d --no-deps
