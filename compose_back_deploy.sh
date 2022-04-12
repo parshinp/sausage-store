d79721f4e39795108108da07b2fac2114a340fb1
Pavel Parshin <parshin@srwd.ru>
2022-04-13 01:03:31 +0300

    compose

--- compose_back_deploy.sh
+++ compose_back_deploy.sh
@@ -9,5 +9,6 @@ VERSION=${VERSION}
 EOF
 sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend:$VERSION
+sudo docker stop backend2 && sudo docker rm backend2
 set -e
 sudo docker-compose --env-file .env.back up -d backend
