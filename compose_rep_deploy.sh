5c94a9e9e9c3a44029744aae3d22610dc769d4e6
Pavel Parshin <parshin@srwd.ru>
2022-04-13 03:49:34 +0300

    report.sh

--- compose_rep_deploy.sh
+++ compose_rep_deploy.sh
@@ -9,5 +9,6 @@ VERSION=${VERSION}
 EOF
 sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend-report:$VERSION
+sudo docker stop report && sudo docker rm report
 set -e
 sudo docker-compose --env-file .env.report up -d backend-report
