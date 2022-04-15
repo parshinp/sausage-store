605cedc4b965f41a6b8b15a027cab081d18bd8d4
Pavel Parshin <parshin@srwd.ru>
2022-04-15 04:58:20 +0300

    lb

--- compose_front_deploy.sh
+++ compose_front_deploy.sh
@@ -9,4 +9,4 @@ sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/frontend:$VERSION
 sudo docker stop loadbalancer && sudo docker rm loadbalancer
 set -e
-sudo docker-compose --env-file .env.front up -d loadbalancer
+sudo docker-compose --env-file .env.front up -d loadbalancer --no-build
