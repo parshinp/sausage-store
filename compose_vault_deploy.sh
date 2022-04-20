fac3b826d0876fb498aaf82662171518186abe3a
Pavel Parshin <parshin@srwd.ru>
2022-04-21 01:01:27 +0300

    host psql

--- compose_vault_deploy.sh
+++ compose_vault_deploy.sh
@@ -11,5 +11,5 @@ vault login ${SPRING_VAULT_TOKEN}
 vault kv put secret/sausage-store spring.datasource.username=${PSQL_USER} \
 spring.datasource.password=${PSQL_PASSWORD} \
 spring.data.mongodb.uri=${MONGO_DATA} \
-spring.datasource.url=${PSQL_HOST}/${PSQL_DBNAME}
+spring.datasource.url=${PSQL_HOST}
 EOF
