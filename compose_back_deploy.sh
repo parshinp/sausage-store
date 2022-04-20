850e9c77c4f9453799bfcf38db639323e79bb9af
Pavel Parshin <parshin@srwd.ru>
2022-04-21 00:41:55 +0300

    8.3

--- compose_back_deploy.sh
+++ compose_back_deploy.sh
@@ -9,6 +9,7 @@ VERSION=${VERSION}
 EOF
 sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend:$VERSION
-sudo docker stop backend2 && sudo docker rm backend2
+#sudo docker stop backend2 && sudo docker rm backend2
 set -e
-sudo docker-compose --env-file .env.back up -d backend
+sudo docker-compose --env-file .env.back up -d green
+sudo docker-compose --env-file .env.back up -d blue
--- compose_vault_deploy.sh
+++ compose_vault_deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 cd /home/jarservice/project/sausage-store
-set +e
+set -e
 
 sudo docker-compose up -d vault
 
 cat <<EOF | sudo docker exec -i vault ash
 sleep 10;
+vault login ${SPRING_VAULT_TOKEN}
 
-vault login myroot
-vault kv put secret/sausage-store spring.datasource.username=sa \
-spring.datasource.password=password \
-spring.data.mongodb.uri=mongodb://parshin:Testusr1234@rc1b-nczpq63snuc5a3z2.mdb.yandexcloud.net:27018/parshin?tls=true \
-spring.datasource.url=jdbc:postgresql://c-c9qslvj961omn1nevnjl.rw.mdb.yandexcloud.net:6432/testdb
+vault kv put secret/sausage-store spring.datasource.username=${PSQL_USER} \
+spring.datasource.password=${PSQL_PASSWORD} \
+spring.data.mongodb.uri=${MONGO_DATA} \
+spring.datasource.url=${PSQL_HOST}/${PSQL_DBNAME}
 EOF
