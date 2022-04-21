2d51e413e7af8579637c0d281db2a86e66e7587f
Pavel Parshin <parshin@srwd.ru>
2022-04-21 20:00:39 +0300

    health.sh

---
+++ health.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /home/jarservice/project/sausage-store
+set +e
+touch .env.back
+cat > .env.back <<EOF
+SPRING_VAULT_HOST=${SPRING_VAULT_HOST}
+SPRING_VAULT_TOKEN=${SPRING_VAULT_TOKEN}
+VERSION=${VERSION}
+EOF
+sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+#sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend:$VERSION
+#sudo docker stop backend2 && sudo docker rm backend2
+set -e
+#sudo docker-compose --env-file .env.back up -d green
+#sudo docker-compose --env-file .env.back up -d blue
+
+if [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" == "healthy"] && ["$( sudo docker container inspect -f '{{.State.Health.Status}}' blue )" == "healthy"]; 
+then [ sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend:$VERSION ] && [ sudo docker-compose stop green ] && [ sudo docker-compose --env-file .env.back up -d green ] && [ sudo docker-compose stop blue ]
+fi
+
