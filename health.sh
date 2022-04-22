e31aece73bd04090be12ad469d616504c88bbbec
Pavel Parshin <parshin@srwd.ru>
2022-04-22 03:37:06 +0300

    health

--- health.sh
+++ health.sh
@@ -5,15 +5,49 @@ touch .env.back
 cat > .env.back <<EOF
 SPRING_VAULT_HOST=${SPRING_VAULT_HOST}
 SPRING_VAULT_TOKEN=${SPRING_VAULT_TOKEN}
-VERSION=${VERSION}
+VERSION=$VERSION
 EOF
+#sudo docker login -u parshin gitlab.praktikum-services.ru:5050 -p
 sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-#sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend:$VERSION
-#sudo docker stop backend2 && sudo docker rm backend2
 set -e
-#sudo docker-compose --env-file .env.back up -d green
-#sudo docker-compose --env-file .env.back up -d blue
 
-if [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" == "healthy"] && ["$( sudo docker container inspect -f '{{.State.Health.Status}}' blue )" == "healthy"]; 
-then [ sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend:$VERSION ] && [ sudo docker-compose stop green ] && [ sudo docker-compose --env-file .env.back up -d green ] && [ sudo docker-compose stop blue ]
+
+# BLUE and GREEN runs
+if [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" == "healthy" ] && [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' blue )" == "healthy" ];
+then sudo docker-compose stop green && sudo docker rm green && sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend$VERSION && sudo docker-compose --env-file=.env.back up -d green
+while [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" != "healthy" ]; do
+        sleep 1;
+done
+sudo docker-compose stop blue; exit;
 fi
+
+
+# BLUE and GREEN stopped
+if [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" == "unhealthy" ] && [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' blue )" == "unhealthy" ];
+then sudo docker rm green && sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend$VERSION && sudo docker-compose --env-file=.env.back up -d green
+while [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" != "healthy" ]; do
+        sleep 1;
+done
+exit;
+fi
+
+
+# BLUE runs GREEN stopped
+if [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" == "unhealthy" ] && [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' blue )" == "healthy" ];
+then sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend$VERSION && sudo docker-compose --env-file=.env.back up -d green
+while [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" != "healthy" ]; do
+        sleep 1;
+done
+sudo docker-compose stop blue; exit;
+fi
+
+
+# BLUE stopped GREEN runs
+if [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' green )" == "healthy" ] && [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' blue )" == "unhealthy" ];
+then sudo docker pull gitlab.praktikum-services.ru:5050/parshin/sausage-store/sausage-backend$VERSION && sudo docker-compose --env-file=.env.back up -d blue
+while [ "$( sudo docker container inspect -f '{{.State.Health.Status}}' blue )" != "healthy" ]; do
+        sleep 1;
+done
+sudo docker-compose stop green; exit;
+fi
+
